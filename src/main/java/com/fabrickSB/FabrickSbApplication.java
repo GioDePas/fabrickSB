292e42e5dc5f72f3cfe696a1b10dc66ae5ef5ea5
GiorgioDePascale <59685505+GiorgioDePascale@users.noreply.github.com>
2023-01-13 01:49:02 +0100

    first commit

---
+++ src/main/java/com/fabrickSB/FabrickSbApplication.java
@@ -0,0 +1,13 @@
+package com.fabrickSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrickSB/FabrickSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrickSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
